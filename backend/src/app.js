6094d2ecc854a183e905d9a5627454e28abcaa87
Thanh <buithanh3786900@gmail.com>
2026-08-12 03:13:06 +0700

    Backend Day3 add post create read update delete by node

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,12 @@ app.use(express.json());
 
 //router import
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 
 // router declaration
 app.use("/api/v1/users", userRouter);
+app.use('/api/v1/posts', postRouter);
+
 
 // example router: http://localhost:4000/api/v1/users/register
 
